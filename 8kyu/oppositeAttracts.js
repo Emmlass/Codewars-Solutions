c2d0b955dacf3127a2bedb606eb41d6b369223a5
Enaibre O. Emmanuel <74358820+Emmlass@users.noreply.github.com>
2024-06-17 17:31:53 +0100

    oppositeAttracts.js

---
+++ 8kyu/oppositeAttracts.js
@@ -0,0 +1,22 @@
+/*
+
+Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each.
+If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+FUNDAMENTALS
+
+*/
+
+function lovefunc(f1, f2){
+  // moment of truth
+  if ((f1%2 === 0 && f2%2 === 0)||(f1%2!==0 && f2%2!==0)){
+    return false
+  }/*else if(f1/2 !==0 && f2/2 ===0){
+    return true
+  }*/else{
+    
+    return true
+  }
+  
+}
